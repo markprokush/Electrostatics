9c3020ae977a5a44ed08fe5b7a3b6e554a5bfb93
markprokush <55581627+markprokush@users.noreply.github.com>
2021-03-14 22:54:07 +0300

    Update SpherePotential.m

--- Dipoles/SpherePotential.m
+++ Dipoles/SpherePotential.m
@@ -1,5 +1,4 @@
-
-function [F,X,Y] = SpherePotential(XYZ,Q,r0,a,b,Dx,Dy,Nxy)
+function [F,X,Y] = SpherePotential(XYZ,Q,R,r0,a,b,Dx,Dy,Nxy)
     n = length(Q);
     e1 = a / ((a(1,1)^2+a(2,1)^2+a(3,1)^2)^0.5);
     c2 = zeros(3,1);
@@ -19,8 +18,11 @@ function [F,X,Y] = SpherePotential(XYZ,Q,r0,a,b,Dx,Dy,Nxy)
     for ii = 1:Nxy(1)
         for jj = 1:Nxy(2)
             for ll = 1:n
-                if 
-                F(ii, jj) =  F(ii, jj) + Q(ll,1) / ((r0(1,1) + X(ii, 1) * e1(1,1) + Y(jj, 1) * e2(1,1) - XYZ(ll,1))^2 + (r0(2,1) + X(ii, 1) * e1(2,1) + Y(jj, 1) * e2(2,1) - XYZ(ll,2))^2 + (r0(3,1) + X(ii, 1) * e1(3,1) + Y(jj, 1) * e2(3,1) - XYZ(ll,3))^2)^(0.5);
+                if (((r0(1,1) + X(ii, 1) * e1(1,1) + Y(jj, 1) * e2(1,1) - XYZ(ll,1))^2 + (r0(2,1) + X(ii, 1) * e1(2,1) + Y(jj, 1) * e2(2,1) - XYZ(ll,2))^2 + (r0(3,1) + X(ii, 1) * e1(3,1) + Y(jj, 1) * e2(3,1) - XYZ(ll,3))^2)^(0.5) < 1)
+                    F(ii, jj) =  F(ii, jj) + Q(ll,1) / R(ll);
+                else
+                    F(ii, jj) =  F(ii, jj) + Q(ll,1) / ((r0(1,1) + X(ii, 1) * e1(1,1) + Y(jj, 1) * e2(1,1) - XYZ(ll,1))^2 + (r0(2,1) + X(ii, 1) * e1(2,1) + Y(jj, 1) * e2(2,1) - XYZ(ll,2))^2 + (r0(3,1) + X(ii, 1) * e1(3,1) + Y(jj, 1) * e2(3,1) - XYZ(ll,3))^2)^(0.5);
+                end
             end
         end
     end    
